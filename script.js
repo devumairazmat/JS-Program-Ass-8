ce0f99c9c81ff916b2d7f67cb250928201e354a4
Umair Azmat <74985334+UMAIR-AZMAT@users.noreply.github.com>
2021-12-30 01:43:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// <!--------------------------------------------------------------------------------------------------------------->
+
+// 1. Write a js program to read and print elements of array.
+
+// var car = [2, 3, 4, "hello", true];
+
+// for (let i = 0; i <= car.length; i++) {
+//   console.log(car[i]);
+// }
+
+// <!--------------------------------------------------------------------------------------------------------------->
+
+// 2. Write a js program to print all negative elements in an array.
+
+// var number = [12, 54, 32, -43, -32, 12, -23];
+// var negativeNumber = number.filter(function (number) {
+//   return number < 0;
+// });s
+// console.log(negativeNumber);
+
+// <!--------------------------------------------------------------------------------------------------------------->
+
+// 3. Write a js program to find sum of all array elements.
+
+// var numberss = [10, 20, 20, 50];
+// let sum = 0;
+// for (let i = 0; i < numberss.length; i++) {
+//   sum = sum + numberss[i];
+// }
+// console.log(sum);
+
+// <!--------------------------------------------------------------------------------------------------------------->
